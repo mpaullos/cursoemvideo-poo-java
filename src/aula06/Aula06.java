d85b65923167b873ef54bb51557e28332145f63f
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2021-12-28 21:47:53 -0300

    exercicios

---
+++ src/aula06/Aula06.java
@@ -0,0 +1,11 @@
+package aula06;
+
+public class Aula06 {
+	public static void main(String[] args) {
+		ControleRemoto controle = new ControleRemoto();
+		controle.maisVolume();
+		controle.play();
+		controle.abrirMenu();
+		controle.fecharMenu();
+	}
+}
